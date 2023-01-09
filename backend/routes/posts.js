4ebd0e8cf7025414f81a3f55c603bf5fb0cd101f
Lucas Santos <LuSantosatl@gmail.com>
2023-01-09 14:17:08 -0500

    file uploading logic

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,20 +1,43 @@
 const express = require("express");
+const multer = require("multer");
 
 const Post = require("../models/post");
 const router = express.Router();
 
-router.post("", (req, res, next) => {
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-    });
-    post.save().then((createdPost) => {
-      res.status(201).json({
-        message: "Post has been added! Great Work!",
-        postId: createdPost.id
-      });
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+router.post("", multer(storage).single("image"), (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((createdPost) => {
+    res.status(201).json({
+      message: "Post has been added! Great Work!",
+      postId: createdPost.id,
     });
   });
+});
 
 router.get("", (req, res, next) => {
   Post.find().then((documents) => {
@@ -55,4 +78,4 @@ router.put("/:id", (req, res, next) => {
   });
 });
 
-module.exports = router
+module.exports = router;
